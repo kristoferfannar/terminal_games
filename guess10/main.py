5b328db0dec7f3aa1a54c1bc6c69da970602d63e
kristoferfannar <kristoferfannarb@gmail.com>
2022-11-22 13:16:49 +0000

    added fizzbuzz - easter egg

--- guess10/main.py
+++ guess10/main.py
@@ -19,69 +19,85 @@ def main():
     display_main()
     while (ui := input().lower()) != 'q':
         if ui == 'h':
-            display_highscores()
+            display_highscores("highscores.txt")
         elif ui == 'r':
             rules()
-        else:
+        elif ui == 'p':
             play()
+        elif ui == 'fizzbuzz':
+            fizzbuzz()
+        else:
+            print(f"{ui} is not a legal command")
+            sleep(2)
         display_main()
     system(CLEAR)
 
 
 def display_main():
-    display_init(5)
+    lines = 6
+    display_init(lines)
     border = '#'*20
-    print(f'{border:^{TERM_X}}')
-    print(f'{"P - Play Game":^{TERM_X}}')
-    print(f'{"R - Rules":^{TERM_X}}')
-    print(f'{"H - Highscores":^{TERM_X}}')
-    print(f'{"Q - Quit":^{TERM_X}}')
-    print(f'{border:^{TERM_X}}')
+    print(f'{border:^{TERM_X}}')  # 1
+    print(f'{"P - Play Game":^{TERM_X}}')  # 2
+    print(f'{"R - Rules":^{TERM_X}}')  # 3
+    print(f'{"H - Highscores":^{TERM_X}}')  # 4
+    print(f'{"Q - Quit":^{TERM_X}}')  # 5
+    print(f'{border:^{TERM_X}}')  # 6
+    display_footer(lines)
 
 
 def display_init(lines=0):
     system(CLEAR)
-    print('\n'*round((TERM_Y-lines) / 2))
+    print('\n'*round((TERM_Y - lines) / 2))
+
+
+def display_footer(lines=0):
+    print('\n'*round((TERM_Y - lines - 3) / 2))
 
 
 def rules():
     quit = False
     while not quit:
-        display_init(5)
-        print("Here you need to press ENTER exactly 10 seconds after the timer starts...")
+        lines = 4
+        display_init(lines)
+        print(
+            "Here you need to press ENTER exactly 10 seconds after the timer starts...")  # 1
         sleep(3)
-        print("The closer you are to 10 seconds, the better your score is...")
+        print("The closer you are to 10 seconds, the better your score is...")  # 2
         sleep(3)
-        print("Will you make it into the top 10?")
-        user = input("q - quit: ").lower()
+        print("Will you make it into the top 10?")  # 3
+        display_footer(lines)
+        user = input("q - quit: ").lower()  # 4
         if user == 'q':
             quit = True
 
 
-def display_highscores():
-    highscores = get_highscores()
+def display_highscores(file):
+    highscores = get_highscores(file)
     print_highscores(highscores)
-    user = input("Q - quit: ")
+    user = input("Q - quit: ")  # 3
 
 
 def print_highscores(highscores):
-    display_init(len(highscores) + 4)
+    lines = len(highscores) + 3
+    display_init(lines)
     border = "#"*50
-    print(f"{border:^{TERM_X}}")
+    print(f"{border:^{TERM_X}}")  # 1
     for index in range(len(highscores)):
         print(
-            f"{index+1:>{round((TERM_X - 40) / 2)}}  |  NAME: {highscores[index][0][:11]:<10}   -  SCORE: {highscores[index][1]:>4}")
-    print(f"{border:^{TERM_X}}")
+            f"{index+1:>{round((TERM_X - 40) / 2)}}  |  NAME: {highscores[index][0][:11]:<10}   -  SCORE: {highscores[index][1]:>4}")  # highscores
+    print(f"{border:^{TERM_X}}")  # 2
+    display_footer(lines)
 
 
-def get_highscores(num=5):
-    scores = get_scores()
+def get_highscores(file, num=5):
+    scores = get_scores(file)
     scores = sorted(scores, key=lambda x: x[1], reverse=True)
     return scores[:num]
 
 
-def get_scores():
-    with open("highscores.txt", 'r') as scores:
+def get_scores(file):
+    with open(file, 'r') as scores:
         all_scores = [score.strip().split(', ')
                       for score in scores.readlines()]
 
@@ -103,7 +119,7 @@ def play():
         f"Your time was {round(user_time, 3)} seconds - {round(difference, 3)} seconds away!")
     print(f"Your score is: {score} points!!!")
 
-    save_score(score)
+    save_score(score, "highscores.txt")
 
 
 def countdown():
@@ -118,10 +134,88 @@ def calculate_score(difference):
     return int((100 / difference))
 
 
-def save_score(score):
+def save_score(score, file):
     name = input("Your name: ")
-    with open("highscores.txt", "a") as scores:
+    with open(file, "a") as scores:
         scores.write(f"{name}, {score}\n")
+
+
+################################
+
+def fb_header(lines=0):
+    system(CLEAR)
+    rows = round((TERM_Y - lines) / 2)
+    print('\n')
+    print(f"{'------------':^{TERM_X}}")
+    print(f"{'| FIZZBUZZ |':^{TERM_X}}")
+    print(f"{'------------':^{TERM_X}}")
+    print('\n'*(rows - 5))
+
+
+def display_fb():
+    lines = 5
+    fb_header(lines)
+    border = '*'*20
+    print(f"{border:^{TERM_X}}")
+    print(f"{'P - Play':^{TERM_X}}")
+    print(f"{'H - Highscores':^{TERM_X}}")
+    print(f"{'Q - Quit':^{TERM_X}}")
+    print(f"{border:^{TERM_X}}")
+    display_footer(lines)
+
+
+def computer_play(num) -> str:
+    if num % 15 == 0:
+        return "fizzbuzz"
+    elif num % 5 == 0:
+        return "buzz"
+    elif num % 3 == 0:
+        return "fizz"
+    else:
+        return str(num)
+
+
+def user_play(num):
+    user_input = input()
+    return computer_play(num) == user_input
+
+
+def fb_calculate_score(total):
+    return int(1000 / total)
+
+
+def fb_play():
+    countdown()
+    start = time()
+
+    for i in range(1, 21):
+        if i % 2 == 1:
+            print(computer_play(i))
+        else:
+            user_success = user_play(i)
+            if not user_success:
+                print("WRONG! - GAME OVER")
+                sleep(3)
+                return
+
+    total = time() - start
+
+    score = fb_calculate_score(total)
+
+    save_score(score, "fizzbuzz.txt")
+
+
+def fizzbuzz():
+    display_fb()
+    while (ui := input().lower()) != 'q':
+        if ui == 'h':
+            display_highscores("fizzbuzz.txt")
+        elif ui == 'p':
+            fb_play()
+        else:
+            print(f"{ui} is not a legal command")
+            sleep(2)
+        display_fb()
 
 
 if __name__ == "__main__":
